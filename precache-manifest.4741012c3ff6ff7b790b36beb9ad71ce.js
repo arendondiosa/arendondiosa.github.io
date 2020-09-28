01d1f666c64c9a4adfe68a8d77474ffc1cf269d3
Alejandro E. Rendon <alejandro@rendon.co>
2020-09-27 20:43:09 -0500

    Updates

---
+++ precache-manifest.4741012c3ff6ff7b790b36beb9ad71ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7308939d4979fe8374e7ef31503cf7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d38f1f73900c61d0f882",
+    "url": "/static/js/2.8463842c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.8463842c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26805897b52ca3e4b263",
+    "url": "/static/js/main.fc862105.chunk.js"
+  },
+  {
+    "revision": "871cff39e0289e8eae84",
+    "url": "/static/js/runtime-main.d7b6b94c.js"
+  }
+]);
