a356b0e130d9bc24a35357889c8c8b2124765882
Alejandro E. Rendon <alejandro@rendon.co>
2021-10-24 16:30:05 -0500

    Test deploy

--- pages/_document.js
+++ pages/_document.js
@@ -16,12 +16,10 @@ class MyDocument extends Document {
           <meta name="twitter:url" content="" />
           <meta name="twitter:card" content="" />
 
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link
-            href="https://fonts.googleapis.com/css?family=Space+Mono"
-            rel="stylesheet"
-          />
-          <link
-            href="https://fonts.googleapis.com/css?family=Kaushan+Script"
+            href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,900&display=swap"
             rel="stylesheet"
           />
         </Head>
