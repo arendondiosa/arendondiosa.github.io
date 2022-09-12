1626b71a1ee6e924242b848b835669aff4eb020d
Alejandro E. Rendon <alejandro@rendon.co>
2022-09-11 20:20:35 -0500

    #20 Refactor navbar to navigate without reload

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { Container, Navbar, Nav } from 'react-bootstrap';
 
-import ThemeToggle from '../../components/ThemeToggle';
+import NavbarItem from './NavbarItem';
+
+// import ThemeToggle from '../../components/ThemeToggle';
+
+const urls = [
+  {
+    name: '🧑🏻‍🦳 ACERCA DE',
+    url: '/about',
+  },
+  {
+    name: 'PROJECTOS',
+    url: '/projects',
+  },
+  {
+    name: 'BLOG',
+    url: '/blog',
+  },
+  {
+    name: 'CONTACTO',
+    url: '/contact',
+  },
+];
 
 export const NavbarSite = () => {
+  const router = useRouter();
+
   return (
     <Navbar
       className="custom-navbar"
@@ -13,28 +38,28 @@ export const NavbarSite = () => {
       sticky="top"
     >
       <Container>
-        <Navbar.Brand className="custom-navbar-link" href="/">
-          ALEJANDRO E. RENDON
-        </Navbar.Brand>
+        <Link href="/">
+          <div className="custom-navbar-link navbar-brand">
+            ALEJANDRO E. RENDON
+          </div>
+        </Link>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav className="me-auto"></Nav>
           <Nav>
-            <Nav.Link className="custom-navbar-link" href="/blog">
-              BLOG
-            </Nav.Link>
-            <Nav.Link className="custom-navbar-link" href="/projects">
-              PROJECTOS
-            </Nav.Link>
-            <Nav.Link className="custom-navbar-link" href="/about">
-              🧑🏻‍🦳 ACERCA DE
-            </Nav.Link>
-            <Nav.Link className="custom-navbar-link" href="/contact">
-              CONTACTO
-            </Nav.Link>
-            <Nav.Link className="custom-navbar-link-button-theme" eventKey={2}>
+            {urls.map((urlItem, idx) => {
+              return (
+                <NavbarItem
+                  key={idx}
+                  url={urlItem.url}
+                  name={urlItem.name}
+                  isActive={router.pathname === urlItem.url}
+                />
+              );
+            })}
+            {/* <Nav.Link className="custom-navbar-link-button-theme" eventKey={2}>
               <ThemeToggle />
-            </Nav.Link>
+            </Nav.Link> */}
           </Nav>
         </Navbar.Collapse>
       </Container>
